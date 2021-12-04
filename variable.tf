865d2172ff99b5fb5f591b13cccd31083e82b94c
Kenechukwu881 <94585147+Kenechukwu881@users.noreply.github.com>
2021-12-04 12:26:36 -0500

    first assignment

---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable "myVPCCidr" {
+  type = string
+  default = "10.1.0.0/16"
+}
+variable "myPriSubCidr" {
+    type = string
+    default = "10.1.1.0/24"
+}
+variable "myPubSubCidr" {
+    type = string
+    default = "10.1.2.0/24"
+}
+variable "myAZ" {
+    type = string
+    default = "us-east-1a"
+  }
+ variable "instance_type" {
+    type = string
+    default = "t2.micro"
+  }
+variable "key_name" {
+    type = string
+    default = "publickeypair"
+  }
+variable "ami" {
+    type = string
+    default = "ami-083654bd07b5da81d"
+  }
+  variable "image_id" {
+      type = string
+      default = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"  
+  }
