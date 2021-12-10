ce106643e065fdcabcdb382e9f057f18f3b09ff4
unknown <ferdinand.tuyishime@kajide.com>
2021-12-10 21:57:26 +0000

    updating my homework project even if all tasks are not finished yet

---
+++ README_solution.md
@@ -0,0 +1,17 @@
+How difficulties were solved
+----------------------------
+Step 1: Cloned the homework directory
+
+Step 2: Created the branch Tuyishime
+
+step 3: Created README_solution.md to explain difficulties
+
+step 4: Made code for creating a bucket
+
+step 5: Create a role to access the bucket
+
+step 6: write a bash script to send files to the bucket from /var/log/ directory from any instance or to send any file to S3 from an instance
+
+step 7: Not encrypted the bucket yet
+
+step 8: Push your code to your branch once you are done
---
+++ main.tf
@@ -0,0 +1,87 @@
+resource "aws_s3_bucket" "Homework-bucket" {
+  bucket = var.s3-bucket-name
+  acl    = "private"
+
+  tags = {
+    Name        = "My homework bucket"
+    Environment = "Study"
+  }
+}
+
+
+resource "aws_iam_role" "s3_fullaccess-role" {
+  name = "homeworkrole"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "s3-fullaccess-role-policy" {
+  name = "s3-fullaccess-policy"
+  role = aws_iam_role.s3_fullaccess-role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "s3fullaccesspolicy-sid",
+      "Action": "*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+})
+}
+
+resource "aws_iam_instance_profile" "instace_profile" {
+    name = "ec2_s3_instance_profile"
+    role = aws_iam_role.s3_fullaccess-role.name
+
+}
+
+
+resource "aws_instance" "Homeworkinstance" {
+    ami = "ami-0d37e07bd4ff37148"
+    instance_type = "t2.micro"
+    key_name = "terrkeypair"
+    iam_instance_profile = aws_iam_instance_profile.instace_profile.name
+    associate_public_ip_address = true
+    user_data = <<EOF
+
+    #!/bin/bash
+
+    yum update -y
+
+    yum install httpd -y
+
+    service httpd start
+
+    chkconfig httpd on
+
+    cd /var/log/
+
+    echo "<html><h1>Hello Cloud Gurus Welcome To My Webpage</h1></html>" >
+
+    index.html
+
+    aws s3 mv . s3://var.s3-bucket-name --recursive --exclude "*.DS_Store"
+
+  EOF
+}
+
