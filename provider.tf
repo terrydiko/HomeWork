640958a161105309be03ba60eb17d96e2aaf4459
unknown <ferdinand.tuyishime@kajide.com>
2021-12-11 22:12:40 +0000

    Adding backend code

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region ="eu-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "kajidestate"
+    key    = "windowsterraform/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "state"
